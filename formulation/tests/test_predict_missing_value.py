90d2d14c7557f20cfee0808cc89f5131593309dc
Dawei Gu <gugugudawei@gmail.com>
2020-03-06 20:09:48 -0800

    Revert "add tests"
    This reverts commit af86f00a88e04ca36b741040a92c7e520f212166.

--- formulation/tests/test_predict_missing_value.py
+++
@@ -1,9 +0,0 @@
-"""
-Test cases for predict_missing_value submodule.
-"""
-import numpy as np
-import pandas as pd
-
-from predict_missing_value import data_dropna
-from predict_missing_value import fill_missing_value
-
