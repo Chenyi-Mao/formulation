866ff26fe1ba380c36a9761ef1b5dc87b4720f51
Zhang Ling <49769108+zhhhling@users.noreply.github.com>
2020-03-08 21:21:55 -0700

    Add files via upload

--- formulation/tests/test_cross_validate.py
+++ formulation/tests/test_cross_validate.py
@@ -1,50 +1,53 @@
-import numpy as np 
-import pandas as pd 
-from formulation.modules.cross_validate import *
+import pandas as pd
+from modules.cross_validate import handle_missing_values
+from modules.cross_validate import cross_validate_grid_search
+# from cross_validate import cross_validate_n_predictors
+
 
 def test_cross_validate_grid_search():
 
-	data_path = "../data/"
-	data_fname = 'FDA_APPROVED.csv'
+    # data_path = "../data/"
+    data_path = "./formulation/data/"
+    data_fname = 'FDA_APPROVED.csv'
 
-	# Read csv file
-	df = pd.read_csv(data_path+data_fname)
-	print(df.tail(3))
+    # Read csv file
+    df = pd.read_csv(data_path+data_fname)
+    print(df.tail(3))
 
-	# Extract columns needed
-	columns = ['CLogP', 'HBA', 'HBD', 'PSDA', 'Formulation']
-	df = df[columns]
+    # Extract columns needed
+    columns = ['CLogP', 'HBA', 'HBD', 'PSDA', 'Formulation']
+    df = df[columns]
 
-	# Handle missing values in selected columns
-	df = handle_missing_values(df)
-	print(df.tail(10))
+    # Handle missing values in selected columns
+    df = handle_missing_values(df)
+    print(df.tail(10))
 
-	# Print count of each category
-	print(df.groupby('Formulation').count())
+    # Print count of each category
+    print(df.groupby('Formulation').count())
 
+    # Prepare predictors and response variable
+    features = ['CLogP', 'HBA', 'HBD', 'PSDA']
+    X_df = df[features]
 
-	# Prepare predictors and response variable
-	features = ['CLogP', 'HBA', 'HBD', 'PSDA']
-	X_df = df[features]
+    target = ['Formulation']
+    y_df = df[target]
 
-	target = ['Formulation']
-	y_df = df[target]
+    max_depth = [1, 2]
+    ntrees = [100]
 
-	max_depth = [1,2]
-	ntrees = [100]
+    values = [max_depth, ntrees]
+    results = cross_validate_grid_search(values, X_df, y_df)
 
-	values = [max_depth, ntrees]
+    assert len(results) == 4
+    assert len(results[0]) == 2
+    assert len(results[1]) == 2
+    assert len(results[2]) == 2
+    assert len(results[3]) == 2
+    m = len(results)
+    n = len(results[0])
+    for i in range(m):
+        for j in range(n):
+            assert isinstance(results[i][j], int)
 
-
-	assert len(cross_validate_grid_search(values, X_df, y_df))==4
-	assert len(cross_validate_grid_search(values, X_df, y_df)[0])==2
-	assert len(cross_validate_grid_search(values, X_df, y_df)[1])==2
-	assert len(cross_validate_grid_search(values, X_df, y_df)[2])==2
-	assert len(cross_validate_grid_search(values, X_df, y_df)[3])==2
-	m = len(cross_validate_grid_search(values, X_df, y_df))
-	n = len(cross_validate_grid_search(values, X_df, y_df)[0])
-	for i in range(m):
-		for j in range(n):
-			assert type(cross_validate_grid_search(values, X_df, y_df)[i][j]) == type(1)
 
 test_cross_validate_grid_search()
