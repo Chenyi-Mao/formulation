32bbd9c9d8bb53d40a25f5441265525a3711e2a1
Dawei Gu <gugugudawei@gmail.com>
2020-03-16 12:10:10 -0700

    changed __init__ structure and supported python versions

--- formulation/__init__.py
+++ formulation/__init__.py
@@ -1,11 +1,3 @@
 __version__ = "1.0"
-# flake8: noqa
-from .modules import classification
-# flake8: noqa
-from .modules import cross_validate
-# flake8: noqa
-from .modules import importance
-# flake8: noqa
-from .modules import predict
-# flake8: noqa
-from .modules import predict_missing_value
+
+from .import modules
---
+++ formulation/modules/__init__.py
@@ -0,0 +1,5 @@
+from . import classification
+from . import cross_validate
+from . import importance
+from . import predict
+from . import predict_missing_value
